7de8d30d5aa3a6f69b57955324a0aa6434fb66d8
alvee-hasan <alveehasan707@gmail.com>
2024-08-19 13:53:14 +0600

    font-awesome updated, my script

--- login-signup.php
+++ login-signup.php
@@ -35,7 +35,7 @@ else if (isset($_POST['lgsubmit'])) {
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/7ce2f708b0.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="style.css" />
   <title>Sign in & Sign up Form</title>
 </head>
